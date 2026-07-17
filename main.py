92b9540b6694fffde2d9e1fcfa3c9be4771e3f87
OlatomiwaOA <olatomiwaolajide2013@gmail.com>
2026-07-17 16:44:41 +0100

    Added a clock.tick and resolved bugs in main.py Added new file circleshape

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
-        for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        dt = clock.tick(60.0)/1000
 
 if __name__ == "__main__":
     main()
